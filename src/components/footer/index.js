048eb17790abcde27a7c9978abf595b50d193687
afcastaneda <afcastaneda1992@live.com>
2023-05-26 21:09:03 -0500

    Informacion Genera

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -12,8 +12,27 @@ const Aux2 = () => {
         <div>
             <ul className="collapsible">
                 <li>
-                <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                <div class="collapsible-header"><i class="material-icons">account_balance</i>Formación académica</div>
+                <div class="collapsible-body">
+                    <span>
+                        <div class="collection">
+                            <a href="#!" class="collection-item active">INGENIERÍA EN SISTEMAS Y TELECOMUNICACIONES
+                            <br />
+                            6to Semestre
+                            <br />
+                            Universidad de Manizales
+                            </a>
+                            <a href="#!" class="collection-item">TECNÓLOGO EN SISTEMAS INFORMÁTICOS
+                            <br />
+                            Culminado 2019
+                            <br />
+                            Universidad de Caldas
+                            </a>
+                            <a href="#!" class="collection-item">Alan</a>
+                            <a href="#!" class="collection-item">Alan</a>
+                        </div>
+                    </span>
+                </div>
                 </li>
                 <li>
                 <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
