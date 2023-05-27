3da2005eca6fcbe3cffbc40cee3dc2250470a31e
afcastaneda <afcastaneda1992@live.com>
2023-05-26 20:54:42 -0500

    Personalizando Pagina

--- src/App.js
+++ src/App.js
@@ -5,21 +5,42 @@ import M from 'materialize-css';
 //Importo el componente
 import Header1 from './components/header'
 import Footer1 from './components/footer'
+import img1 from './components/img/1.jpg';
 
 function App() {
   return (
     <div className="App">
       
-      {/*Llamando el componente Header*/}
-      <Header1></Header1>
-        <div className="card">
-          <div className="card-content"></div>
-          <h2>Probando</h2>
+      <div className="row">
+        <div className="col s2">
+          <div className="card">
+            <div className="card-image">
+              <img src={img1} id='myImage'/>
+              <span class="card-title">Andrés Felipe Castañeda</span>
+            </div>
+            <div class="card-content">
+              <p>Mi formación académica, humana y laboral se ha enfocado al desarrollo
+                  y la implementación de propuestas útiles en el campo de la
+                  productividad e innovación de ideas. Tengo experiencia en manejo de
+                  personal. Soy un profesional comprometido con la investigación, el
+                  liderazgo y el trabajo en equipo. Soy enfocado al logro, con metas claras
+                  y precisas y busco a la mayor brevedad todos los recursos posibles para
+                  llegar a ésta.</p>
+            </div>
+          </div>
+          
         </div>
-        <a class="waves-effect waves-light btn">button</a>
+        <div className='col s10'>
+        <Footer1/>
+        </div>
+        
+      </div>
+
+      {/*Llamando el componente Header*/}
+      
         
         {/*Llamando el componente Footer*/}
-      <Footer1/>
+      
     </div>
     
   );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import M from 'materialize-css';
 
 //Se genera componente
 const Aux = () => {
+    useEffect(() => {
+        // Inicialización del collapsible cuando el componente se monta
+        var elems = document.querySelectorAll('.parallax');
+        M.Collapsible.init(elems);
+      }, []);
+
     return(
         <div>
-            <h1>Header</h1>
+            
+            
         </div>
     );
 };
