d3aa3ab6de3e82fb9f7d08b6a0a163ad0813aa3c
afcastaneda <afcastaneda1992@live.com>
2023-05-26 21:45:21 -0500

    Finish

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -95,8 +95,41 @@ const Aux2 = () => {
                     </div>
                 </li>
                 <li>
-                <div class="collapsible-header teal lighten-4"><i class="material-icons">whatshot</i>Third</div>
-                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    <div class="collapsible-header teal lighten-4"><i class="material-icons">whatshot</i>Contacto</div>
+                    <div class="collapsible-body">
+                        <span>
+                            <div class="row ">
+                                <form class="col s12 teal lighten-5">
+                                <div class="row ">
+                                    <div class="input-field col s6">
+                                    <input placeholder="Placeholder" id="first_name" type="text" class="validate"/>
+                                    <label for="first_name">First Name</label>
+                                    </div>
+                                    <div class="input-field col s6">
+                                    <input id="last_name" type="text" class="validate"/>
+                                    <label for="last_name">Last Name</label>
+                                    </div>
+                                </div>                                
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                    <input id="password" type="password" class="validate"/>
+                                    <label for="password">Asunto</label>
+                                    </div>
+                                    <div class="input-field col s6">
+                                    <input id="last_name" type="text" class="validate"/>
+                                    <label for="last_name">Mensaje</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                    <input id="email" type="email" class="validate"/>
+                                    <label for="email">Email</label>
+                                    </div>
+                                </div>
+                                </form>
+                            </div>
+                        </span>
+                    </div>
                 </li>
             </ul>
         </div>        
