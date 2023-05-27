6666cae9d09cdb5e9338b948b20404cd27b20cc5
afcastaneda <afcastaneda1992@live.com>
2023-05-26 21:28:56 -0500

    Experiencia Laboral

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,37 +9,93 @@ const Aux2 = () => {
       }, []);
 
     return(
-        <div>
-            <ul className="collapsible">
+        <div className=''>
+            <ul className="collapsible teal lighten-2">
                 <li>
-                <div class="collapsible-header"><i class="material-icons">account_balance</i>Formación académica</div>
-                <div class="collapsible-body">
-                    <span>
+                    <div class="collapsible-header teal lighten-4"><i class="material-icons">account_balance</i>Formación académica</div>
+                    <div class="collapsible-body ">
+                        <span>
+                            <div class="collection">
+                                <a href="#!" class="collection-item active"><b>INGENIERÍA EN SISTEMAS Y TELECOMUNICACIONES</b>
+                                <br />
+                                6to Semestre
+                                <br />
+                                Universidad de Manizales
+                                </a>
+                                <a href="#!" class="collection-item"><b>TECNÓLOGO EN SISTEMAS INFORMÁTICOS</b>
+                                <br />
+                                Culminado 2019
+                                <br />
+                                Universidad de Caldas
+                                </a>
+                                <a href="#!" class="collection-item"><b>DIPLOMADO MISION TIC FUNDAMENTOS DE PROGRAMACIÓN</b>
+                                <br />
+                                Culminado 2022
+                                <br />
+                                Ministerio de las Telecomunicaciones - Universidad Tecnológica de Pereira
+                                </a>
+                                <a href="#!" class="collection-item"><b>BACHILLER ACADÉMICO CON ÉNFASIS EN MERCADEO</b>
+                                <br />
+                                Culminado 2009
+                                <br />
+                                Colegio Eugenio Pacelli
+                                </a>
+                            </div>
+                        </span>
+                    </div>
+                </li>
+                <li>
+                    <div class="collapsible-header teal lighten-4"><i class="material-icons">work</i>Experiencia Profesional</div>
+                    <div class="collapsible-body">
+                        <span>
                         <div class="collection">
-                            <a href="#!" class="collection-item active">INGENIERÍA EN SISTEMAS Y TELECOMUNICACIONES
-                            <br />
-                            6to Semestre
-                            <br />
-                            Universidad de Manizales
-                            </a>
-                            <a href="#!" class="collection-item">TECNÓLOGO EN SISTEMAS INFORMÁTICOS
-                            <br />
-                            Culminado 2019
-                            <br />
-                            Universidad de Caldas
-                            </a>
-                            <a href="#!" class="collection-item">Alan</a>
-                            <a href="#!" class="collection-item">Alan</a>
-                        </div>
-                    </span>
-                </div>
+                                <a href="#!" class="collection-item active"><b>Consultor de automatización</b>
+                                <br />
+                                Comdata -  Konecta 7 meses - actualmente
+                                <br />
+                                <p>Revisar y garantizar todo el proceso de trascodificacion y transcripción
+                                de los segmentos que actualmente se tienen a cargo en el área, revisar el
+                                funcionamiento correcto y dar soporte a los diferentes bots de descarga
+                                y carga de llamadas que se tienen, realizar diferentes desarrollos según
+                                las necesidades del area.</p>
+                                </a>
+                                <a href="#!" class="collection-item"><b>Consultor de Analytics</b>
+                                <br />
+                                Comdata Group 18 meses
+                                <br />
+                                <p>Por medio de herramientas de business intelligence, servicios de cloud
+                                computing y diferentes lenguajes de programación se elaboran
+                                dashboards que permiten realizar una labor de consultoría aportando a
+                                los clientes una correcta toma de decisiones y/o acciones a nivel
+                                operativo buscando incrementar indicadores de gestión, de atención y de
+                                calidad.</p>
+                                </a>
+                                <a href="#!" class="collection-item"><b>Reporting</b>
+                                <br />
+                                UNO27 12 meses
+                                <br />
+                                <p>Creación, diseño y ejecución de pago de comisiones acorde al
+                                rendimiento de los empleados, elaboración de informes periódicos sobre
+                                rendimiento de servicios de ventas y atención al cliente con el fin de
+                                detectar falencias y crear alarmas operativas, control de personal,
+                                creación y ejecución de planes de acción para incrementar productividad
+                                e indicadores de rendimiento.</p>
+                                </a>
+                                <a href="#!" class="collection-item"><b>Técnico de conducción</b>
+                                <br />
+                                Emergia 24 meses
+                                <br />
+                                <p>Elaboración de informes periódicos sobre rendimiento de los empleados
+                                para una entidad financiera, manejo de personal, creación de alertas
+                                operativas, diseño y ejecución de planes de acción para incrementar
+                                productividad e indicadores de rendimiento.</p>
+                                </a>
+                            </div>
+                        </span>
+                    </div>
                 </li>
                 <li>
-                <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                </li>
-                <li>
-                <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+                <div class="collapsible-header teal lighten-4"><i class="material-icons">whatshot</i>Third</div>
                 <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
                 </li>
             </ul>
